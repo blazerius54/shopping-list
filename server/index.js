2cbb64ecb29a3930d507d1af6914d0d6dee78ec2
Maxim Stepanov1 <Maxim_Stepanov1@epam.com>
2019-07-07 21:10:42 +0300

    server/boilerplate: added mongo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
 
 const app = express();
-
 const port = 5000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(port, ()=>console.log(`Server starterd on port ${port}`));
+app.listen(port, ()=>console.log(`Server starterd on port ${port}`, config.mongoURI));
+
+mongoose.connect(
+  config.mongoURI, 
+  { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+db.once('open', () => console.log('mongo is running',));
