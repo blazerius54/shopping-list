fb2538e7813496d15973b9a2e98e66ae76a34e18
Maxim Stepanov1 <Maxim_Stepanov1@epam.com>
2019-08-18 18:06:53 +0300

    server/add socketio

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import socketIOClient from "socket.io-client";
 
+const socket = socketIOClient.connect("http://localhost:5050");
 const App = () => {
   const [products, setProducts] = useState([]);
-  const fetchProducts = () => {
-    return fetch("http://localhost:5000/products/all")
-        .then(items => items.json())
-        .then(result => setProducts(result))
+  // const fetchProducts = () => {
+  //   return fetch("http://localhost:5000/products/all")
+  //       .then(items => items.json())
+  //       .then(result => setProducts(result))
+  // };
+
+
+  // useEffect(() => {
+  //   console.log('mnt')
+  //
+  //   socket.emit("initial_data");
+  //   socket.on("get_data", getData);
+  // });
+
+  const getData = (data) => {
+    console.log("data", data);
+    // socket.emit("getProducts");
+    setProducts(data)
+  };
+
+  const deleteProduct = id => {
+    console.log(id)
+    socket.emit("delete_item", id);
+    socket.on("get_data", getData);
+  }
+
+  const foo = () => {
+    socket.emit("initial_data");
+    socket.on("get_data", getData);
   };
 
   return (
       <>
         It`s your app
-        <button onClick={fetchProducts}>click</button>
-        <button onClick={()=>socketIOClient.connect("http://localhost:5050")}>fire socket</button>
+        {/*<button onClick={fetchProducts}>click</button>*/}
+        <button onClick={foo}>foo</button>
+        {/*<button onClick={()=>socket.connect("http://localhost:5050")}>connect socket</button>*/}
+        {/*<button onClick={()=>socket.on("getProducts")}>get lists</button>*/}
+        {/*<button onClick={()=>socketIOClient.disconnect()}>disconnect socket</button>*/}
         {
           products.length > 0 && (
-              products.map(({name}, index) => (
+              products.map(({name, _id}, index) => (
                   <div key={index} style={{display: "flex"}}>
                     <p>{name}</p>
-                    <button>delete</button>
+                    <button onClick={() => deleteProduct(_id)}>delete</button>
                   </div>
               ))
           )
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const test = require("./models/Products");
 const cors = require("cors");
 const socket = require("socket.io");
 const ProductsModel = test.ProductsModel;
+const ShoppingListModel = require('./models/ShoppingList');
+
 
 const app = express();
 const port = 5000;
@@ -15,18 +17,48 @@ app.use(cors());
 app.use("/lists", require("./routes/lists"));
 app.use("/products", require("./routes/products"));
 
+const getProducts = () => {
+  ProductsModel.find({})
+      .then((products) => {
+        console.log(products)
+        io.emit("get_data", products);
+      })
+}
+
 // socket setup
 const io = socket(5050);
 io.on("connection", (socket) => {
-  console.log('SOCKET', socket.id);
+  console.log("user have connected", socket.id);
+
+  io.on("disconnect", () => {
+    console.log("user have disconnected")
+  });
+
+  // ProductsModel.find({}, (err, products) => {
+  //   io.emit("products", products);
+  // });
+
+  socket.on("initial_data", getProducts);
+
+  socket.on("delete_item", (id) => {
+    console.log(id)
+    ProductsModel
+        .findById(id)
+        .then(item => {
+          item.remove()
+              .then(()=> getProducts())
+          // io.emit("get_data", products);
+        })
+  })
+
 });
 
 
-app.listen(port, ()=>console.log(`Server started on port ${port}`, config.mongoURI));
+app.listen(port, () => console.log(`Server started on port ${port}`, config.mongoURI));
 
 mongoose.connect(
-  config.mongoURI,
-  { useNewUrlParser: true }
+    config.mongoURI,
+    {useNewUrlParser: true}
 );
 
 db.once("open", () => console.log("mongo is running"));
