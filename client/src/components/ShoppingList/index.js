a76533e50638d1f65fd1b4e4bc8838199be03af6
blazerius54 <79137837220@yandex.ru>
2019-10-20 17:01:28 +0300

    feature/new_shopping_list: refactor

--- client/src/components/ShoppingList/index.js
+++ client/src/components/ShoppingList/index.js
@@ -3,19 +3,16 @@ import List from "@material-ui/core/List";
 import {ShoppingListWrapper} from "./styles";
 import ShoppingListItem from "./ShoppingListItem";
 
-const ShoppingList = ({productsForList, handleProductTypeChange}) => {
-
-  return (
-    <ShoppingListWrapper>
-      <List disablePadding>
-        <h2>List</h2>
-        {productsForList.length > 0 &&
-        productsForList.map((product, index) => (
-          <ShoppingListItem product={product} index={index} handleProductTypeChange={handleProductTypeChange}/>
-        ))}
-      </List>
-    </ShoppingListWrapper>
-  )
-};
+const ShoppingList = ({productsForList, handleProductTypeChange}) => (
+  <ShoppingListWrapper>
+    <List disablePadding>
+      <h2>List</h2>
+      {productsForList.length > 0 &&
+      productsForList.map((product, index) => (
+        <ShoppingListItem product={product} index={index} handleProductTypeChange={handleProductTypeChange}/>
+      ))}
+    </List>
+  </ShoppingListWrapper>
+);
 
 export default ShoppingList;
