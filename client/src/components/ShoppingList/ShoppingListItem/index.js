2de208a3e629f36551325cbd63e77e1e9db163cb
Maxim Stepanov1 <Maxim_Stepanov1@epam.com>
2019-11-16 18:33:44 +0300

    new_shoppong_list: fix styles

--- client/src/components/ShoppingList/ShoppingListItem/index.js
+++ client/src/components/ShoppingList/ShoppingListItem/index.js
@@ -5,7 +5,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
-import {useStyles} from "./styles";
+import {useStyles, ControlsWrapper} from "./styles";
 
 const ShoppingListItem = ({product, handleProductTypeChange, index}) => {
   const changeProductType = (event) => {
@@ -19,30 +19,32 @@ const ShoppingListItem = ({product, handleProductTypeChange, index}) => {
 
   const classes = useStyles();
   return (
-    <ListItem key={product.name} button>
+    <ListItem key={product.name} className={classes.itemWrapper} button>
       <ListItemText className={classes.inputLabel} primary={product.name}/>
-      <TextField
-        id="standard-number"
-        label="количество"
-        onChange={handleAmountChange}
-        type="number"
-        className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        margin="normal"
-      />
-      <FormControl
-        className={classes.select}
-      >
-        <Select
-          value={product.type || "шт"}
-          onChange={changeProductType}
+      <ControlsWrapper>
+        <TextField
+          id="standard-number"
+          label="количество"
+          onChange={handleAmountChange}
+          type="number"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          // margin="normal"
+        />
+        <FormControl
+          className={classes.select}
         >
-          <MenuItem value={"шт"}>шт</MenuItem>
-          <MenuItem value={"кг"}>кг</MenuItem>
-        </Select>
-      </FormControl>
+          <Select
+            value={product.type || "шт"}
+            onChange={changeProductType}
+          >
+            <MenuItem value={"шт"}>шт</MenuItem>
+            <MenuItem value={"кг"}>кг</MenuItem>
+          </Select>
+        </FormControl>
+      </ControlsWrapper>
     </ListItem>
   )
 };
--- client/src/components/ShoppingList/ShoppingListItem/styles.js
+++ client/src/components/ShoppingList/ShoppingListItem/styles.js
@@ -1,15 +1,25 @@
 import {makeStyles} from "@material-ui/core";
+import styled from "styled-components";
+
+export const ControlsWrapper = styled.div`
+  display: flex;
+`;
 
 export const useStyles = makeStyles(theme => ({
+  itemWrapper: {
+    display: "flex",
+  },
   inputLabel: {
     alignSelf: "stretch",
     display: "flex",
     alignItems: "flex-end",
+    flex: "0 0 60%",
   },
   textField: {
     marginBottom: 0,
   },
   select: {
     alignSelf: "flex-end",
+    flex: "0 0 40%",
   }
 }));
